9eba089ab4b58ff8c073050c7aeb62e4feff6e34
Yogas Karnik <yogas.karnik@openbravo.com>
2022-02-08 07:38:23 +0100

    Added events

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -27,15 +27,19 @@ contract Allowance is Ownable {
 
 contract SharedWallet is Allowance {
 
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
+
     function withdrawMoney(address payable _to, uint _amount) public isAllowed(_amount) {
         require(_amount <= address(this).balance, "Contract does'nt own enough money");
         if(!isOwner()){
             reduceAllowance(msg.sender, _amount);
         }
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
 
     receive() external payable {
-
+        emit MoneyReceived(msg.sender, msg.value);
     }
 }
